ad5a07b003653cca21ddf94207cbd526a5eb87e6
prithwiscu <prithwi.chakraborty@scu.edu.au>
2024-07-03 13:54:59 +1000

    Base class commit
    This is the base and empty Student class.

---
+++ gradingsystem/Student.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student
+{
+    public static void main(String[] args) {
+        
+    }
+}
